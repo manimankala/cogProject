438572572057dc95ba6a809d1e44301c58deb97e
manimankala <manikantam4444@gmail.com>
2025-04-09 17:19:29 +0530

    updated prime nos program

--- Main.java
+++ Main.java
@@ -4,7 +4,7 @@ class Main{
         printPrime(10);
     }
     public static void printPrime(int n){
-        for(int i=2;i<=n;i++){
+        for(int i=2;i<=Math.sqrt(n);i++){
             int count=0;
             for(int j=1;j<=n;j++){
                 if(i%j==0){
