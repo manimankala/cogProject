3cea4fd6ad25dc92e9c8323717a0bdb2880da94a
manimankala <manikantam4444@gmail.com>
2025-04-09 15:54:58 +0530

    added primeNos code

---
+++ Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+class Main{
+    public static void main(String[] args){
+        printPrime(10);
+    }
+    public static void printPrime(int n){
+        for(int i=2;i<=n;i++){
+            int count=0;
+            for(int j=1;j<=n;j++){
+                if(i%j==0){
+                    count++;
+                }
+            }
+            if(count==2){
+                System.out.print(i+" ");
+            }
+        }
+    }
+}
