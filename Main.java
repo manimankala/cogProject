098c3f9b14d8e1196a15e0bd4f3bb7d933331877
manimankala <manikantam4444@gmail.com>
2025-04-14 23:18:30 +0530

    modified Main.java

--- Main.java
+++ Main.java
@@ -2,18 +2,38 @@ import java.util.Scanner;
 class Main{
     public static void main(String[] args){
         printPrime(10);
+        int n=1;
+        if(isPrime(n)){
+            System.out.println(n+"isPrime");
+        }
+        else{
+            System.out.println(n+" isNotPrime");
+        }
     }
     public static void printPrime(int n){
         for(int i=2;i<=n;i++){
-            int count=0;
-            for(int j=1;j<=n;j++){
+            boolean isPrime=true;
+            for(int j=2;j<=Math.sqrt(i);j++){
                 if(i%j==0){
-                    count++;
+                    isPrime=false;
+                    break;
                 }
             }
-            if(count==2){
-                System.out.print(i+" ");
+            if(isPrime){
+                System.out.println(i+" ");
             }
         }
+    }
+
+    public static boolean isPrime(int n){
+        if(n<=1){
+            return false;
+        }
+        for (int i=2;i<=Math.sqrt(n);i++){
+            if(n%i==0){
+                return false;
+            }
+        }
+            return true;
     }
 }
